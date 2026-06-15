3676f3412b65e78366f16a5619bba1acd68b4afe
Ivo Blok <ivoblokdoorn@gmail.com>
2026-06-15 20:03:56 +0200

    added example main file for collecting average velocity across parameter range

--- src/main.py
+++ src/main.py
@@ -1,17 +1,24 @@
 import numpy as np
 import time
-
+import matplotlib.pyplot as plt
 from environment import Environment
 from model import Model
 
-environment = Environment(n_lanes=2, lane_length=5, v_max=5, n_agents=2)
-model = Model(environment=environment, slowdown=0.05)
+slowdown_probabilities = np.linspace(0, 1, 15)
+average_relative_speed = []
+for p in slowdown_probabilities:
+    # todo sample same parameter multiple times
+    environment = Environment(n_lanes=1, lane_length=100, v_max=5, n_agents=10)
+    model = Model(environment=environment, slowdown=p)
 
-start_time = time.time()
-for _ in range(10000):
-    # print(model.environment.positions)
-    # print(model.environment.velocity)
-    # print("------------------------")
-    model.step()
+    for _ in range(100):
+        # print(model.environment.positions)
+        # print(model.environment.velocity)
+        # print("------------------------")
+        model.step()
+    average_relative_speed.append(np.average(model.environment.velocity) / model.environment.v_max)
 
-print("total time taken: ", time.time() - start_time)
+plt.plot(slowdown_probabilities, average_relative_speed)
+plt.xlabel("slowdown probability")
+plt.ylabel("average speed")
+plt.show()
